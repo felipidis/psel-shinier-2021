612ac87e994ea4eeacbcf15b4705437a98a406e2
felipidis <felipekdra@gmail.com>
2021-04-08 12:49:22 -0300

    Header e Sidebar montados

--- shinier-app/src/components/User.vue
+++ shinier-app/src/components/User.vue
@@ -1,37 +1,49 @@
 <template>
-  <div class="hero has-background-black">
-    <div class="hero-body">
-      <div class="is-flex-desktop is-justify-content-space-between is-align-items-center">
-        <div class="is-flex-desktop is-align-items-center " >
-          <img src="../assets/Logo_analytics.png">
-          <b-field >
-            <b-input placeholder="Buscar Funcionalidades" ></b-input>
-          </b-field>
-        </div>
-        <div class="">
-          <p class="has-text-grey-lighter has-text-weight-bold">
-            Super Admin
-          </p>
-        </div>
-      </div>
-      <div class="is-flex is-justify-content-space-between is-align-items-center">
-        <p class="is-size-4 has-text-weight-bold has-text-grey-lighter">Usuários com acesso a plataforma</p>
-        <b-button class="m-3  " type="is-success">Novo Usuário</b-button>
-      </div>
-      
-      <div class="container is-rounded has-background-dark is-rounded is-flex is-justify-content-space-between ">
-        <div class="is-flex">
-          <img class="m-2" src="../assets/Filter.png" alt="Icone do filtro">
-          <h1 class="m-2 has-text-grey-lighter ml-2" >Filtro | ordenar por</h1>
-        </div>
-        <div>
-          <b-button type="is-black">Data</b-button>
-          <b-button type="is-black">Nome</b-button>
-          <b-button type="is-black">Atualização</b-button>
-        </div>
-        
-      </div>
+  <div class="hero has-background-black is-fullheight">
+    <div class="hero-head is-flex is-align-items-center mt-3 ml-3">
+      <img src="../assets/Logo_analytics.png" alt="Logo da Analytics" />
+      <div class="container is-flex is-justify-content-space-between">
+        <b-field>
+          <b-input
+            placeholder="Buscar Funcionalidades"
+            type="search"
+            icon="magnify"
+            class="text-input"
+          >
+          </b-input>
+        </b-field>
 
+        <p class="has-text-white mr-6 mt-2">Super Admin</p>
+      </div>
+    </div>
+
+    <div class="tile mx-5 mb-2 mt-6">
+      <div class="tile is-parent is-2 ">
+        <article class="tile is-child">
+          <div class="content is-flex is-flex-direction-column">
+            <div class=" is-flex is-align-items-center">
+              <img src="../assets/sidemenu/Dash.png" alt="">
+              <span class="m-3 ml-4 has-text-white">Dashboard</span>
+            </div>
+            <div class=" is-flex is-align-items-center">
+              <img src="../assets/sidemenu/Group.png" alt="">
+              <span class="m-2 has-text-white">Grupos</span>
+            </div>
+            <div class=" is-flex is-align-items-center">
+              <img src="../assets/sidemenu/User.png" alt="">
+              <span class="m-3 ml-5 has-text-white">Usuários</span>
+            </div>
+            <div class=" is-flex is-align-items-center">
+              <img src="../assets/sidemenu/System.png" alt="">
+              <span class="m-3 ml-5 has-text-white">Sistema</span>
+            </div>
+            <div class=" is-flex is-align-items-center">
+              <img src="../assets/sidemenu/Categorie.png" alt="">
+              <span class="m-3 ml-5 has-text-white">Categorias</span>
+            </div>
+          </div>
+        </article>
+      </div>
     </div>
   </div>
 </template>
@@ -43,3 +55,10 @@ export default {
   }
 }
 </script>
+
+<style>
+.text-input {
+  width: 30.625rem;
+  height: 2.5rem;
+}
+</style>
