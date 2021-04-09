61e047e75e88a4f4221ea00f7acc10ca205ee2e5
felipidis <felipekdra@gmail.com>
2021-04-09 17:02:03 -0300

    Projeto Finalizado
    Alguns detalhes não estão idênticos, usarei o tempo restante para tentar melhorar a página.

--- shinier-app/src/views/Cadastro.vue
+++ shinier-app/src/views/Cadastro.vue
@@ -19,32 +19,133 @@
 
       <div class="tile is-parent is-10 p-2 mb-1">
         <article class="tile is-child has-background-dark box">
-          <div class="content is-flex is-flex-direction-column">
-            <div>
-              <Header class="is-flex is-justify-content-space-between">
-                <div class="is-flex is-align-items-center">
-                  <img class="mx-2" src="../assets/Filter.png" alt="" />
-                  <span class="has-text-grey-light"> Filtros </span>
-                  <img class="mx-3" src="../assets/Line.png" alt="" />
-                  <span class="has-text-grey-light"> ordenar por </span>
+          <div class="tile is-ancestor">
+            <div class="tile is-parent">
+              <article class="tile is-child">
+                <div class="field">
+                  <label class="label has-text-grey-light">Nome</label>
+                  <div class="control">
+                    <input
+                      class="input"
+                      type="text"
+                      placeholder="Text input"
+                      required
+                    />
+                  </div>
                 </div>
-                <div class="buttons has-addons">
-                  <button class="button is-black is-selected">Yes</button>
-                  <button class="button is-black has-background-black-bis">
-                    Maybe
-                  </button>
-                  <button class="button is-black has-background-black-bis">
-                    No
-                  </button>
+              </article>
+            </div>
+            <div class="tile is-parent">
+              <article class="tile is-child">
+                <div class="field">
+                  <label class="label has-text-grey-light">Email</label>
+                  <div class="control">
+                    <input
+                      class="input"
+                      type="email"
+                      placeholder="Email"
+                      required
+                    />
+                  </div>
                 </div>
-              </Header>
-
-              <main>
-                <div class="is-flex is-align-items-center">
-                  <p class="p-2 has-text-grey-light">Usuários</p>
-                  <hr class="hr has-background-grey" />
+              </article>
+            </div>
+          </div>
+          <div class="tile is-ancestor">
+            <div class="tile is-parent">
+              <article class="tile is-child">
+                <div class="field">
+                  <label class="label has-text-grey-light">Senha</label>
+                  <div class="control">
+                    <input
+                      class="input"
+                      type="password"
+                      placeholder="*********"
+                      required
+                    />
+                  </div>
                 </div>
-              </main>
+              </article>
+            </div>
+            <div class="tile is-parent">
+              <article class="tile is-child">
+                <div class="field">
+                  <label class="label has-text-grey-light">Repetir Senha</label>
+                  <div class="control">
+                    <input
+                      class="input"
+                      type="password"
+                      placeholder="*********"
+                    />
+                  </div>
+                </div>
+              </article>
+            </div>
+            <div class="tile is-parent">
+              <article class="tile is-child">
+                <div class="field">
+                  <label class="label has-text-grey-light">CPF</label>
+                  <div class="control">
+                    <input
+                      class="input"
+                      type="text"
+                      placeholder="999.999.999-99"
+                    />
+                  </div>
+                </div>
+              </article>
+            </div>
+            <div class="tile is-parent">
+              <article class="tile is-child">
+                <div class="field">
+                  <label class="label has-text-grey-light">Telefone</label>
+                  <div class="control">
+                    <input
+                      class="input"
+                      type="tel"
+                      placeholder="(DD) 99999-9999"
+                      required
+                    />
+                  </div>
+                </div>
+              </article>
+            </div>
+          </div>
+          <div class="tile is-ancestor">
+            <div class="tile is-parent">
+              <article class="tile is-child">
+                <div class="field">
+                  <label class="label has-text-grey-light">Grupo</label>
+                  <b-field>
+                    <b-select
+                      placeholder="Super Admin - cargo -  Pessoas responsáveis com todas as funcionalidades"
+                      expanded
+                    >
+                      <option value="Super">Super Admin</option>
+                      <option value="Admin">Admin</option>
+                    </b-select>
+                  </b-field>
+                </div>
+              </article>
+            </div>
+          </div>
+          <div>
+            <p class="has-text-grey-light">Grupos Extras</p>
+            <div class="tile is-ancestor mt-2">
+              <div class="tile is-parent">
+                <article class="tile is-child is-2">
+                  <div class="field">
+                    <label class="label has-text-grey-light">Campo 1</label>
+                    <div class="control">
+                      <input
+                        class="input"
+                        type="password"
+                        placeholder="*********"
+                      />
+                    </div>
+                  </div>
+                </article>
+              </div>
             </div>
           </div>
         </article>
@@ -59,7 +160,7 @@ import Sidebar from "@/components/Sidebar.vue";
 import FaixaCentral from "@/components/FaixaCentral.vue";
 
 export default {
-  name: "Cadastro",
+  name: "",
   props: {},
   components: {
     Navbar,
@@ -80,8 +181,5 @@ export default {
   margin-top: 12px;
   height: 1px;
   border: none;
-}
-::-webkit-scrollbar {
-  display: none;
 }
 </style>
